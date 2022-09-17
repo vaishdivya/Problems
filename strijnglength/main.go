8bcec38e502177b7ea0edab08adb389b9dfb718b
Divyayaramachu <divyayaramachu01@gmail.com>
2022-09-17 11:38:41 -0400

    different version

--- strijnglength/main.go
+++ strijnglength/main.go
@@ -2,15 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
-	cities := []string{"Mumbai", "Hyderabad", "Calicut", "Chennai"}
-	targetlength := 9
+func citiesGreaterThan(city []string, targetvalue int) int {
 	count := 0
-	for _, value := range cities {
-		if len(value) >= targetlength {
+	for _, value := range city {
+		if len(value) >= targetvalue {
 			count = count + 1
 		}
 	}
+	return count
+}
 
-	fmt.Println(count)
+func main() {
+	cities := []string{"Mumbai", "Hyderabad", "Calicut", "Chennai"}
+	targetlength := 9
+	fmt.Println(citiesGreaterThan(cities, targetlength))
 }
